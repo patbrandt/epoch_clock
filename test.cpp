7e22117750324de1d1b05a7bbe0028ddbbb61608
Patrick Brandt <patbrandt@gmail.com>
2019-01-15 20:56:41 -0700

    Correct CORBA -> Gregorian.
    Fun fact: CORBA uses the epoch of the Gregorian calendar.

--- test.cpp
+++ test.cpp
@@ -11,26 +11,26 @@ using JulianClock = EpochClock<
 template <>
 const typename JulianClock::duration JulianClock::epoch{2440587.5};
 
-// A clock that provides the current CORBA time
-// CORBA time is in units of 100ns, and has an epoch
-// of midnight on 15 October, 1582 CE
-using CorbaClock = EpochClock<
+// A clock that provides the current Gregorian time
+// Gregorian time in units of 100ns. The Gregorian calendar
+// has an epoch of midnight on 15 October, 1582 CE
+using GregorianClock = EpochClock<
     std::chrono::duration<uint64_t, std::ratio<1, 10000000> > >;
 template <>
-const typename CorbaClock::duration CorbaClock::epoch{0x1b21dd213814000};
+const typename GregorianClock::duration GregorianClock::epoch{0x1b21dd213814000};
 
 int main() {
     auto current_julian_time = JulianClock::now();
     std::cout << "Current Julian time = " << std::fixed
               << current_julian_time.time_since_epoch().count() << std::endl;
 
-    auto current_corba_time = CorbaClock::now();
-    std::cout << "Current CORBA time = "
-              << current_corba_time.time_since_epoch().count() << std::endl;
+    auto current_greg_time = GregorianClock::now();
+    std::cout << "Current Gregorian time = "
+              << current_greg_time.time_since_epoch().count() << std::endl;
 
-    CorbaClock::time_point time{CorbaClock::duration(137000856413280000ULL)};
+    GregorianClock::time_point time{GregorianClock::duration(137000856413280000ULL)};
     std::cout << "Converting " << time.time_since_epoch().count() << " to system time = "
-              << CorbaClock::to_time_t(time) << std::endl;
+              << GregorianClock::to_time_t(time) << std::endl;
 
     return 0;
 }
